b3bd06e57514625517d5236b52b87f077a0be51f
konghouyin <1360234119@qq.com>
2020-04-07 00:44:39 +0800

    fix

--- src/server/Server.java
+++ src/server/Server.java
@@ -2,7 +2,7 @@ package server;
 
 public class Server {
 	public static void main(String[] args) {
-		System.out.println("Hello world!--development!!!");
+		System.out.println("Hello world!--development!!er阿萨德!");
 		System.out.println("Hello world!--development!!!0000");
 		System.out.println("Hello world!--development!!!");
 	}
