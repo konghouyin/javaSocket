a4a68ba89f0396748403ffb4b983830efb0d28eb
konghouyin <1360234119@qq.com>
2020-04-07 01:05:03 +0800

    fix

--- src/server/Server.java
+++ src/server/Server.java
@@ -4,6 +4,6 @@ public class Server {
 	public static void main(String[] args) {
 		System.out.println("Hello world!--development!!er阿萨德!");
 		System.out.println("Hello world!--development!!!0000");
-		System.out.println("Hello world!--development!!!");
+		System.out.println("Hello world!--development!!!sad");
 	}
 }
