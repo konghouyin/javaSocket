6f39ff8e64c0bb35bd9461ad69863c4db8f0e82a
konghouyin <1360234119@qq.com>
2020-04-07 00:02:59 +0800

    add git add .

--- src/server/Server.java
+++ src/server/Server.java
@@ -2,6 +2,6 @@ package server;
 
 public class Server {
 	public static void main(String[] args) {
-		System.out.println("Hello world!--development");
+		System.out.println("Hello world!--development!!!");
 	}
 }
