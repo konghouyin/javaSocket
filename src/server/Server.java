13f7ced635b6be08ebfee2494e4ff367870e02a0
konghouyin <1360234119@qq.com>
2020-04-07 01:37:11 +0800

    fix

--- src/server/Server.java
+++ src/server/Server.java
@@ -3,7 +3,5 @@ package server;
 public class Server {
 	public static void main(String[] args) {
 		System.out.println("Hello world!--development!!er阿萨德!");
-		System.out.println("Hello world!--development!!!0000");
-		System.out.println("Hello w手动阀撒的发生orld!--development!!!sad");
 	}
 }
