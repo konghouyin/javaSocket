c4bbdde0446052b43ff5198c4c88f1d0d652bf05
konghouyin <1360234119@qq.com>
2020-04-08 12:01:00 +0800

    git触发jenkins测试

--- src/server/Server.java
+++ src/server/Server.java
@@ -3,5 +3,7 @@ package server;
 public class Server {
 	public static void main(String[] args) {
 		System.out.println("Hello world!--development!!er阿萨德!");
+
+		System.out.println("Hello world!--de		build description:([^@]*)@@([^@]*)@@([^@']*)velopment!!er阿萨德!");
 	}
 }
