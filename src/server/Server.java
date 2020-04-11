03e96f621f5823658cb854ab129fcaed15bc651f
konghouyin <1360234119@qq.com>
2020-04-11 17:37:35 +0800

    test-yarong

--- src/server/Server.java
+++ src/server/Server.java
@@ -2,8 +2,6 @@ package server;
 
 public class Server {
 	public static void main(String[] args) {
-		System.out.println("Hello world!--development!!er阿萨德!");
-
-		System.out.println("Hello world!--de		build description:([^@]*)@@([^@]*)@@([^@']*)velopment!!er阿萨德!");
+		System.out.println("Hello world!yarong!");
 	}
 }
